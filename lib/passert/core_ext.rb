96b677a20f9f9eb27fed806d075aab56ee22e17c
Chris Hanks <christopher.m.hanks@gmail.com>
2016-09-01 14:01:48 -0400

    Also clean backtraces when asserted through Kernel.

--- lib/passert/core_ext.rb
+++ lib/passert/core_ext.rb
@@ -5,5 +5,9 @@ AssertionFailed = Passert::AssertionFailed
 module Kernel
   def assert(*args, &block)
     Passert.assert(*args, &block)
+  rescue => e
+    # Remove this method from the backtrace, since it's not helpful.
+    e.backtrace.shift
+    raise e
   end
 end
